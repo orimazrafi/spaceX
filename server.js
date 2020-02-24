d729ba0843f9de7365c4365838c9c1cf9d8fc65f
ori mazrafi <orimazrafi@gmail.com>
2020-02-24 06:03:44 +0200

    Finish react

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
 const schema = require('./schema.js')
 const app = express();
+const path = require('path')
+app.use(cors())
+
 app.use("/graphql", graphqlHTTP({
     schema,
     graphiql: true
 }));
+
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 
 const PORT = process.env.PORT || 5000;
 
